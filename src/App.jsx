643a01e6f35a5a7cc88a953d5575b42ec548a79f
abu-shaif-khan <askhan@rhrk.uni-kl.de>
2024-02-20 14:37:58 +0100

    4.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-export default function App(){
-    return (
-        
-    );
+import { useState } from "react";
+import "./App.css";
+import CatFriends from "./components/CatFriends";
+export default function App() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <div>
+      {show && <CatFriends />}
+      <br />
+      <button onClick={() => setShow((s) => !s)}>
+        {show ? "Hide" : "Show"}
+      </button>
+    </div>
+  );
 }
-
-
