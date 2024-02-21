ec26ac351da1f5b0f3fb802e792ad82b5fae441f
abu-shaif-khan <askhan@rhrk.uni-kl.de>
2024-02-21 13:36:07 +0100

    4.4

--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -1,15 +1,13 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const MyInput = forwardRef((props, ref) => {
+  const realInputRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    focus() {
+      realInputRef.current.focus();
+    },
+  }));
+  
   return <input {...props} ref={ref} />;
 });
 export default MyInput;
-// import { forwardRef } from "react";
-
-// const MyInput = function (props, ref) {
-//     return <input {...props} ref={ref} />;
-// };
-
-// const ForwardedMyInput = forwardRef(MyInput);
-
-// export default ForwardedMyInput;
