b1076b864793d86377c508bbb7990354ff41ba9c
abu-shaif-khan <askhan@rhrk.uni-kl.de>
2024-03-02 20:18:29 +0100

    3.20 (todo with redurcer and context fixed bug)

--- src/App.jsx
+++ src/App.jsx
@@ -8,40 +8,13 @@ import taskReducer from "./reducers/taskReducer.js";
 
 export default function TaskApp() {
   const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
-  const getNextId = (data) => {
-    const maxId = data.reduce((prev, current) =>
-      prev && prev.id > current.id ? prev.id : current.id
-    );
-    return maxId + 1;
-  };
-  const handleAddTask = (text) => {
-    dispatch({
-      type: "Added",
-      text: text,
-      id: getNextId(tasks),
-    });
-  };
-
-  const handleChangeTask = (task) => {
-    dispatch({
-      type: "Changed",
-      task: task,
-    });
-  };
-
-  const handleDeleteTask = (taskId) => {
-    dispatch({
-      type: "Deleted",
-      id: taskId,
-    });
-  };
 
   return (
     <>
       <TaskContext.Provider value={tasks}>
         <TaskDispatchContext.Provider value={dispatch}>
           <h1>Prague itinerary</h1>
-          <AddTask onAdd={handleAddTask} />
+          <AddTask />
           <TaskList />
         </TaskDispatchContext.Provider>
       </TaskContext.Provider>
